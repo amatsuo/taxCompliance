37b5c6d95029ed6a42adfc81248fbf8b5a835d63
José Orellana <jose.orellanan@usach.cl>
2016-02-04 21:00:33 -0300

    view Module5 dataPlayer complete

--- views/contexts/en/dataPlayer.js
+++ views/contexts/en/dataPlayer.js
@@ -8,6 +8,12 @@ module.exports = function(settings, headers) {
         text3:"Sexo:",
         text4:"Femenino",
         text5:"Masculino",
+        text6:"En política se habla normalmente de \"izquierda\" y \"derecha\". En una escala donde \"0\" es la \"izquierda\"   y \"10\" es la \"derecha\", ¿Dónde se ubicaría usted? ",
+        text7:"Izquierda   ",
+        text8:"   Derecha",
+        text9:"En general, ¿Diría que se puede confiar en la mayoría de las personas o uno nunca es lo suficientemente cuidadoso en el trato con los demás? ",
+        text10:"Se puede confiar en la mayoría de las personas",
+        text11: "uno nunca es lo suficientemente cuidadoso en el trato con los demás",
         proceed:"Continuar",
         error:"Debe ingresar los datos solicitados antes de continuar."
     };
